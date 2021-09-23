4e73b5c4c8a4ef7af8da34d72cd833e846263037
Utoben <86472697+Utoben@users.noreply.github.com>
2021-09-23 14:57:35 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('1772039101:AAHpISe0rckraCTeTTmha2xwGAQ7h8TcIFY')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
+
+# Знаки процентов убираем
